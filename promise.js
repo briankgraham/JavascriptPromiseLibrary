9aeaacd3bef7b17342045aa93ea281947c93c373
Brian Graham <graham.briank@gmail.com>
2016-02-09 15:24:28 -0800

    Promises retain return values in then function

--- promise.js
+++ promise.js
@@ -1,30 +1,87 @@
-var Promise = function (func) {
-  var state = 'PENDING', 
-      value, 
-      deferred;
-  
-  this.then = function (cb) {
-    handle(cb);
-  }
+// TO DO: ALLOW FOR MORE THAN OTHER DEFERRED 
+function handle(promise, val) {
+  var item, deferred;
 
-  func(resolve);
-  
-  function resolve (newValue) {
-    value = newValue;
-    state = 'RESOLVED';
-
+  promise.result = val;
+  while (promise.queue.length) {
+    item = promise.queue.shift();
+    if (item.state === 'REJECTED') {
+      deferred = item.errback;
+    } else {
+      deferred = item.callback;
+    }
     if (deferred) {
-      handle(deferred);
+      try {
+        promise.result = deferred(promise.result);
+      } catch (e) {
+        promise.state = 'REJECTED';
+        promise.result = e;
+      }
     }
   }
 
-  function handle (onResolved) {
-    if (state === 'PENDING') {
-      deferred = onResolved;
-      return;
-    }
+}
 
-    onResolved(value);
-  }
+var Promise = function (func) {
+  var that = this;
+  this.state = 'PENDING';
 
+  this.queue = [];
+
+  func(function (value) {
+    that.resolve(value)
+  });
 };
+
+Promise.prototype.then = function (callback, errback) {
+  this.queue.push({ callback: callback, errback: errback });
+  if (this.state !== 'PENDING') {
+    handle(this, this.result);
+  }
+  return this;
+}
+
+Promise.prototype.resolve = function (newValue) {
+  // check if Rejected / Fulfilled
+  console.log(this.state)
+  if (this.state === 'REJECTED') {
+    throw new Error('Promise has been rejected');
+  }
+  if (this.state === 'FULFILLED') {
+    throw new Error('Promise can only be fulfilled once');
+  }
+  this.result = newValue;
+  this.state = 'RESOLVED';
+  handle(this, newValue);
+};
+
+Promise.prototype.reject = function (error) {
+  // check if Rejected / Fulfilled
+  if (this.state === 'REJECTED') {
+    throw new Error('Promise has been rejected');
+  }
+  if (this.state === 'FULFILLED') {
+    throw new Error('Promise can only be fulfilled once');
+  }
+  this.result = newValue;
+  this.state = 'REJECTED';
+
+  handle(this, error);
+};
+
+
+var Promise = new Promise(function (resolve, reject) {
+  resolve(5)
+});
+
+
+console.log(Promise.then(function (res) {
+  console.log('first: ' , res)
+  return res + 1;
+}).then(function (resp) {
+  console.log('resp: ', resp)
+  return resp + 1;
+}).then(function (news) {
+  console.log('news: ', news)
+}))
+// console.log(Promise)
